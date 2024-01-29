5956a3fe626cf366a661431c2d68a95bd3aacba9
howdy-hanna <137307806+howdy-hanna@users.noreply.github.com>
2024-01-28 19:52:36 -0500

    updated HTML for Items
    took out input and put in placer prices to test jQuery functions

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -1,9 +1,29 @@
+// calculate cost per item
 $(document).ready(function () {
   $('tbody tr').each(function (i, ele) {
     var itemPrice = parseFloat($(ele).children('.price').text());
     var itemQty = parseFloat($(ele).children('.qty').text());
 
+    // total cost is price times  qty per item
     var totalCost = itemPrice * itemQty;
     $(ele).children('.cost').html(totalCost);
   });
 });
+
+    
+
+
+
+//remove item from cart
+
+
+//add item to cart
+
+
+//update cart when qty changes
+
+
+
+
+
+
