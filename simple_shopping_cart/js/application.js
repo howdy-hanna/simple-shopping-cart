2a0d2240197fdd13754716aa31dc4c3aa8568f41
howdy-hanna <137307806+howdy-hanna@users.noreply.github.com>
2024-01-30 19:57:37 -0500

    tried to do JQuery
    tried to add code to update cart total - did not work

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -1,13 +1,21 @@
 // calculate cost per item
 $(document).ready(function () {
+
+//find cart cost
+$('tbody').on('change', 'input.qty', function () {
+  // calculat subtotal and updat the corresponding .cost <td> with the new number.
   $('tbody tr').each(function (i, ele) {
     var itemPrice = parseFloat($(ele).children('.price').text());
-    var itemQty = parseFloat($(ele).children('.qty').text());
+    var itemQty = parseFloat($(ele).find('.qty input').val());
 
     // total cost is price times  qty per item
     var totalCost = itemPrice * itemQty;
     $(ele).children('.cost').html(totalCost);
+
+    return totalCost;
   });
+})
+
 
   //remove item from cart
 $('.btn.remove').on('click', function (event) {
@@ -21,7 +29,7 @@ $('.btn.remove').on('click', function (event) {
 //find cart cost
 var sum = function (acc, x) { return acc + x; };
 
-var cartCost = [];
+var totalCost = [];
 
 
 
