97b27f38ea87fb7dafe4562febb2aae896479552
howdy-hanna <137307806+howdy-hanna@users.noreply.github.com>
2024-02-02 11:05:53 -0500

    put in updated code

--- simple_shopping_cart/js/application.js
+++ simple_shopping_cart/js/application.js
@@ -1,46 +1,48 @@
 // calculate cost per item
-$(document).ready(function () {
+$(document).ready(function() {
 
-//find cart cost
-$('tbody').on('change', 'input.qty', function () {
-  // calculat subtotal and updat the corresponding .cost <td> with the new number.
-  $('tbody tr').each(function (i, ele) {
-    var itemPrice = parseFloat($(ele).children('.price').text());
-    var itemQty = parseFloat($(ele).find('.qty input').val());
+  //update sub total on input change
+  $('tbody').on('change', '.qty input', function() {
+    // calculat subtotal and updat the corresponding .cost <td> with the new number.
+    var row = $(this).closest('tr');
+    var qty = parseFloat($(this).val());
+    var cost = parseFloat(row.find('td.price').text());
+    var subtotal = qty * cost;
 
-    // total cost is price times  qty per item
-    var totalCost = itemPrice * itemQty;
-    $(ele).children('.cost').html(totalCost);
-
-    return totalCost;
+    row.find('td.cost').text(subtotal);
+    updateCartTotal();
   });
-})
+
+  var updateCartTotal = function() {
+    var cartTotal = 0;
+    $('tbody tr').each(function(i, ele) {
+      cartTotal += parseFloat($(ele).find('td.cost').text()) || 0;
+    });
+
+    $('.cart-total span').html(cartTotal);
+  }
+
+  //add item to cart
+  $('#addItem').on('submit', function(event) {
+    event.preventDefault();
+    var name = $(this).children('[name=name]').val();
+    var price = $(this).children('[name=price]').val();
+    var qty = $(this).children('[name=qty]').val();
+
+    $('tbody').append('<tr>' +
+      '<td class="item">' + name + '</td>' +
+      '<td class="price"><input type="number" value="' + price + '" /></td>' +
+      '<td class="qty"><input type="number" value="' + quantity + '" /></td>' +
+      '<td class="cost"></td>' +
+      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+      '</tr>');
+  });
 
 
   //remove item from cart
-$('.btn.remove').on('click', function (event) {
-  $(this).closest('tr').remove();
-  //add part to update total cost 
-});
+  $('.btn.remove').on('click', function(event) {
+    $(this).closest('tr').remove();
+    //add part to update total cost 
+  });
 
 });
-
-    
-//find cart cost
-var sum = function (acc, x) { return acc + x; };
-
-var totalCost = [];
-
-
-
-
-//add item to cart
-
-
-//update cart when qty changes
-
-
-
-
-
-
